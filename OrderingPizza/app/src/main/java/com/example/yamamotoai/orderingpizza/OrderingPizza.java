07186bb5fd77a221a48f0be0169d0595141061fc
AiYamamoto <ay36230@gmail.com>
2017-07-10 12:09:18 -0700

    Create a project Delete 2 projects(SampleProject1,2)

---
+++ OrderingPizza/app/src/main/java/com/example/yamamotoai/orderingpizza/OrderingPizza.java
@@ -0,0 +1,17 @@
+package com.example.yamamotoai.orderingpizza;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by yamamotoai on 2017-07-10.
+ */
+
+public class OrderingPizza extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.orderingpizza);
+    }
+}
--- SampleProject2/app/src/main/java/com/example/ciccc_cirac/sampleproject2/PizzaOrder.java
+++
@@ -1,8 +0,0 @@
-package com.example.ciccc_cirac.sampleproject2;
-
-/**
- * Created by CICCC-CIRAC on 2017-07-07.
- */
-
-public class PizzaOrder {
-}
