b4c32658fe419624f0c2b8f961bf707cca53a2d4
AiYamamoto <ay36230@gmail.com>
2017-07-19 12:32:09 -0700

    update

--- RecyclerViewExample2/app/src/main/java/com/example/yamamotoai/recyclerviewexample2/MainActivity.java
+++ RecyclerViewExample2/app/src/main/java/com/example/yamamotoai/recyclerviewexample2/MainActivity.java
@@ -4,12 +4,21 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private List<Movie> movieList = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private MovieAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +35,13 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycleview);
+        mAdapter = new MovieAdapter(movieList);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setAdapter(mAdapter);
+        prepareMovieData();
     }
 
     @Override
@@ -49,4 +65,38 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void prepareMovieData() {
+        Movie movie = new Movie("Inside Out", "Animation, Kids & Family", "2015");
+        movieList.add(movie);
+        movie = new Movie("Mad Max: Fury Road", "Action & Adventure", "2015");
+        movieList.add(movie);
+        movie = new Movie("Passengers", "Action & Adventure", "2016");
+        movieList.add(movie);
+        movie = new Movie("Inception", "Action", "2010");
+        movieList.add(movie);
+        movie = new Movie("Titanic", "Romance", "1997");
+        movieList.add(movie);
+        movie = new Movie("Inside Out", "Animation, Kids & Family", "2015");
+        movieList.add(movie);
+        movie = new Movie("Mad Max: Fury Road", "Action & Adventure", "2015");
+        movieList.add(movie);
+        movie = new Movie("Passengers", "Action & Adventure", "2016");
+        movieList.add(movie);
+        movie = new Movie("Inception", "Action", "2010");
+        movieList.add(movie);
+        movie = new Movie("Titanic", "Romance", "1997");
+        movieList.add(movie);
+        movie = new Movie("Inside Out", "Animation, Kids & Family", "2015");
+        movieList.add(movie);
+        movie = new Movie("Mad Max: Fury Road", "Action & Adventure", "2015");
+        movieList.add(movie);
+        movie = new Movie("Passengers", "Action & Adventure", "2016");
+        movieList.add(movie);
+        movie = new Movie("Inception", "Action", "2010");
+        movieList.add(movie);
+        movie = new Movie("Titanic", "Romance", "1997");
+        movieList.add(movie);
+    }
+
 }
